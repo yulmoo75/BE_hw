6ed4665a7860463b611495084b93fda3b897a5e6
yulmoo75 <yulmoo0705@gmail.com>
2026-04-06 21:19:53 +0900

    [0406] 천유진

--- blog/blog_prj/blog_prj/settings.py
+++ blog/blog_prj/blog_prj/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog', 
+    'users',
+    'accounts', 
 ]
 
 MIDDLEWARE = [
@@ -116,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'users.User'
