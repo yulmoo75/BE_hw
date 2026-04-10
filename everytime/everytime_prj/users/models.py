0f7f4cd20bf0162746db3231e520051ece500b47
yulmoo75 <yulmoo0705@gmail.com>
2026-04-10 19:42:12 +0900

    [0410] 천유진

---
+++ everytime/everytime_prj/users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+    university = models.CharField(max_length=50)
+    email = models.CharField(max_length=30, unique=True, null=False, blank=False)
+    nickname = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.username
