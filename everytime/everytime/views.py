47aebc7bbff10250595f2c49c8baedff28accae2
yulmoo75 <yulmoo0705@gmail.com>
2026-04-11 22:20:22 +0900

    [0411] 천유진

---
+++ everytime/everytime/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from .models import Post
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def list(request):
+    posts = Post.objects.all().order_by('-id')
+    return render(request, 'blog/list.html', {'posts': posts})
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+
+        post = Post.objects.create(
+            title = title,
+            content = content
+        )
+        return redirect('blog:list')
+    return render(request, 'blog/create.html')
+
+def detail(request, id):
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'blog/detail.html', {'post':post})
+
+def update(request, id):
+    post = get_object_or_404(Post, id=id)
+
+    if request.method == 'POST':
+        post.title = request.POST.get('title')
+        post.content = request.POST.get('content')
+        post.save()
+        return redirect('blog:detail', id)
+    return render(request, 'blog/update.html', {'post':post})
+
+def delete(request, id):
+    post = get_object_or_404(Post, id=id)
+    post.delete()
+    return redirect('blog:list')
